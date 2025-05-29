466ae1953a800f314bb613c21463fe5b7bed721e
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-29 07:13:42 -0500

    now opens maximized

--- procview/src/main.rs
+++ procview/src/main.rs
@@ -1,7 +1,9 @@
 use eframe::{self, egui};
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions::default();
+    let mut options = eframe::NativeOptions::default();
+    options.viewport.maximized = Some(true);
+
     eframe::run_simple_native("Rust Bucket", options, |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("this is a label chuckle duck")
