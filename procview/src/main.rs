171f3cf5646a3a41c3b76c2f4a3a6dc0af6dc2d4
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-29 06:50:48 -0500

    the gui works

--- procview/src/main.rs
+++ procview/src/main.rs
@@ -1,5 +1,10 @@
-use eframe;
+use eframe::{self, egui};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_simple_native("Rust Bucket", options, |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("this is a label chuckle duck")
+        });
+    })
 }
