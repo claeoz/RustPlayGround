2652dd006de6da77119aef18918f9e3fd411fb20
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-29 20:15:14 -0500

    slight tidying

--- procview/src/main.rs
+++ procview/src/main.rs
@@ -1,12 +1,40 @@
 use eframe::{self, egui};
 
+//holds the base data like fields
+struct BaseData {
+    counter: u32
+}
+
+//inititializes the BaseData
+impl Default for BaseData {
+    fn default() -> Self {
+        Self { counter: 0 }
+    }
+}
+
+//definies the gui
+impl eframe::App for BaseData {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(format!("Count: {}", self.counter));
+
+            if ui.button("Increment").clicked() {
+                self.counter += 1;
+            }
+        });
+    }
+}
+//this is the main function
 fn main() -> Result<(), eframe::Error> {
+
     let mut options = eframe::NativeOptions::default();
+
     options.viewport.maximized = Some(true);
 
-    eframe::run_simple_native("Rust Bucket", options, |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("this is a label chuckle duck")
-        });
-    })
+    //launches app
+    eframe::run_native(
+        "Rust Bucket", 
+        options, 
+        Box::new(|_cc| Box::new(BaseData::default())),
+    )
 }
